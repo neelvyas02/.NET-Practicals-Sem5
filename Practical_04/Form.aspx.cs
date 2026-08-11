f8f30bf09aad2f8332dd00c6619417b3847e4415
Neel Vyas <neel.vyas133231@marwadiuniversity.ac.in>
2026-08-11 08:33:39 +0530

    Add project files.

---
+++ Practical_04/Form.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Practical_04
+{
+    public partial class Form : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ValidationSettings.UnobtrusiveValidationMode =
+                System.Web.UI.UnobtrusiveValidationMode.None;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (!Page.IsValid)
+            {
+                return;
+            }
+
+            string department = RadioButtonList1.SelectedValue;
+
+            string gender = RadioButtonList2.SelectedValue;
+
+            string skills = "";
+
+            if (CheckBox1.Checked)
+            {
+                skills += "C# ";
+            }
+
+            if (CheckBox2.Checked)
+            {
+                skills += "Python ";
+            }
+
+            if (CheckBox3.Checked)
+            {
+                skills += "AI ";
+            }
+
+            string terms = "Accepted";
+
+            Label11.Text =
+                "<b>Submitted Details</b><br/><br/>" +
+                "Full Name: " + TextBox1.Text + "<br/>" +
+                "Email Id: " + TextBox2.Text + "<br/>" +
+                "Contact No: " + TextBox3.Text + "<br/>" +
+                "College: " + TextBox4.Text + "<br/>" +
+                "Department: " + department + "<br/>" +
+                "Event: " + DropDownList1.SelectedValue + "<br/>" +
+                "Gender: " + gender + "<br/>" +
+                "Skills: " + skills + "<br/>" +
+                "Address: " + TextBox5.Text + "<br/>" +
+                "Terms: " + terms;
+        }
+    }
+}
